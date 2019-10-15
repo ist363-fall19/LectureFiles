0281e4bfe2ff99fdce10958c0059d6a9defb2d0d
Nick Lyga <ndlyga@syr.edu>
2019-10-15 09:47:19 -0400

    updated code

--- Lecture10/site.js
+++ Lecture10/site.js
@@ -1,9 +1,12 @@
 
+// Data types
 var name = "Bob"; // String
 var age = 42; // Number
 var siblings = ["Mary", "Sue", "John"]; // Array
 var isWorking = true; // Boolean
 
+
+// Function Example
 function print(message) {
     document.write("<p>" + message + "</p>");
 }
@@ -13,10 +16,12 @@ for (var i = 0 ; i < siblings.length ; i++) {
     print(siblings[i]);
 }
 
-
+// String concat
 var msg = name + " is " + age.toString() + " years old";
-// print(msg); // Call function;
 
+print(msg); // Call function;
+
+// Complex Object
 var person = {
     name: "Bob",
     age: 42,
@@ -30,7 +35,23 @@ var person = {
     }
 }
 
-// alert(person.name);
+// if Statements
+if (true) { // Condition
+    // Do this
+} else if (true) { // or some other condition
+    // Do this
+} else {
+    // Do this
+}
 
 
+// Loops
+for (var i = 0 ; i < length ; i++) {
+    // do this over and over
+}
 
+// Event Listenter
+var el = document.getElementById("MyButton")
+el.addEventListener("click", function(evt) {
+    // Do something when the event happens
+})

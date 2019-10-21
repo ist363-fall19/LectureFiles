5030642dffb34e1a2fa92e510b3216a8fa241db9
Nick Lyga <ndlyga@syr.edu>
2019-10-21 16:22:26 -0400

    added tip calc example

---
+++ TipCalculator/tip.js
@@ -0,0 +1,17 @@
+// Variables
+var formValid = false;
+
+// Event Listeners
+
+
+
+// Function to validate bill amount
+function validateTotal(evt) {
+
+}
+
+
+// Function to calculate tip amount and display the tip amount!
+function totalBill() {
+
+}
